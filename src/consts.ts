2a561e4f3725713cf757be7e40d033f106423de0
Your Name <you@example.com>
2026-04-21 12:04:09 +0800

    refactor: auto-sync SITE_VERSION from package.json

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,5 @@
+import { version } from '../package.json';
+
 export const SITE_TITLE = 'Word';
 export const SITE_DESCRIPTION = 'A blog at word.247base.uk';
-export const SITE_VERSION = '0.4.0';
+export const SITE_VERSION = version;
